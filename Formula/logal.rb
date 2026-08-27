2f5c2698cde7eaa451af21a7164e55bcf5cd35f0
Luca Candela <73209+CaliLuke@users.noreply.github.com>
2026-08-27 13:57:34 -0700

    Update Logal to v0.2.0

--- Formula/logal.rb
+++ Formula/logal.rb
@@ -1,14 +1,14 @@
 class Logal < Formula
   desc "Local OpenTelemetry collector backed by disposable SQLite"
   homepage "https://github.com/CaliLuke/autok-logal"
-  url "https://github.com/CaliLuke/autok-logal/releases/download/v0.1.0/autok-logal-0.1.0.tar.gz"
-  sha256 "92fd37e1495c6c3723225ac0b9be082ed5635c1c5095315aa39fb440f002baa9"
+  url "https://github.com/CaliLuke/autok-logal/releases/download/v0.2.0/autok-logal-0.2.0.tar.gz"
+  sha256 "212642d8caf6a72d5ef28fba9714f810a0fcf12ab5b6656f76b402f2022be136"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    ldflags = "-s -w -X main.version=#{version}"
+    ldflags = "-X main.version=#{version}"
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/logal"
   end
 
