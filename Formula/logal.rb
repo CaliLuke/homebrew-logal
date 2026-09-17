5bc6f0ac49c3428cf55bb255ac67b3b3990bf3d5
Luca Candela <luca.candela@gmail.com>
2026-09-16 21:52:13 -0700

    Update Logal to corrective release v0.3.1

--- Formula/logal.rb
+++ Formula/logal.rb
@@ -1,8 +1,8 @@
 class Logal < Formula
   desc "Local OpenTelemetry collector backed by disposable SQLite"
   homepage "https://github.com/CaliLuke/autok-logal"
-  url "https://github.com/CaliLuke/autok-logal/releases/download/v0.3.0/autok-logal-0.3.0.tar.gz"
-  sha256 "ac207d71c2e4ad63d901926b30acb8ebde9924b49e532b1a3ce04b4ebb0a74a2"
+  url "https://github.com/CaliLuke/autok-logal/releases/download/v0.3.1/autok-logal-0.3.1.tar.gz"
+  sha256 "093c928f191a021d92017ef6e5c0aa37cb26ce569836e606a60624accc097c6d"
   license "MIT"
 
   depends_on "go" => :build
