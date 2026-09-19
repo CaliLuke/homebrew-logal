4db6d3f0b61c5d717611bba987151eac807691de
Luca Candela <luca.candela@gmail.com>
2026-09-19 15:12:42 -0700

    logal 0.5.0

--- Formula/logal.rb
+++ Formula/logal.rb
@@ -1,8 +1,8 @@
 class Logal < Formula
   desc "Local OpenTelemetry collector backed by disposable SQLite"
   homepage "https://github.com/CaliLuke/autok-logal"
-  url "https://github.com/CaliLuke/autok-logal/releases/download/v0.4.0/autok-logal-0.4.0.tar.gz"
-  sha256 "f2128a00aacea0f009c2b3401581be7593584da3c1397cc17cfe009b7226edbc"
+  url "https://github.com/CaliLuke/autok-logal/releases/download/v0.5.0/autok-logal-0.5.0.tar.gz"
+  sha256 "81aa7fb8527cac7ae037d9712b1b7e204300f7f525e51e59b19690840decc122"
   license "MIT"
 
   depends_on "go" => :build
